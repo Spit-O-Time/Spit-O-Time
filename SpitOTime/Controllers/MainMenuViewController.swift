798e7ad8b28b201e2b47badba41a016769484651
viniciusmesquita <=>
2021-04-07 11:28:13 -0300

    Add audio menu

--- SpitOTime/Controllers/MainMenuViewController.swift
+++ SpitOTime/Controllers/MainMenuViewController.swift
@@ -9,40 +9,40 @@ import UIKit
 
 class MainMenuViewController: UIViewController {
 
-    
-    
     @IBOutlet weak var sound: UIButton! {
         didSet {
             sound.layer.masksToBounds = false
             sound.layer.cornerRadius = 8
         }
     }
+
     @IBOutlet weak var music: UIButton! {
         didSet {
             music.layer.masksToBounds = false
             music.layer.cornerRadius = 8
         }
     }
+
     @IBOutlet weak var play: UIButton! {
         didSet {
             play.layer.masksToBounds = false
             play.layer.cornerRadius = 16
         }
     }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
 
-    }
-    
-    
+    let audioManager = AudioManager()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+            self.audioManager.playSound(named: .menuBackground, numberOfLoop: -1, volume: 0.5)
+        }
         setButtonImage(forKey: .isSoundEffectMuted, button: sound)
         setButtonImage(forKey: .isSoundtrackMuted, button: music)
         navigationController?.setNavigationBarHidden(true, animated: false)
     }
-    
+
     @IBAction func playButtonAction(_ sender: Any) {
         let controller = GameViewController()
         let transition = CATransition()
@@ -52,14 +52,15 @@ class MainMenuViewController: UIViewController {
         navigationController?.view.layer.add(transition, forKey: kCATransition)
         navigationController?.pushViewController(controller, animated: false)
     }
+
     @IBAction func changeSoundtrackAction(_ sender: Any) {
         changeValueUserDefaults(forKey: .isSoundtrackMuted, button: music)
     }
-    
+
     @IBAction func changeSoundEffectAction(_ sender: Any) {
         changeValueUserDefaults(forKey: .isSoundEffectMuted, button: sound)
     }
-    
+
     func changeValueUserDefaults(forKey: AudioConfig, button: UIButton) {
         if UserDefaults.standard.bool(forKey: forKey.rawValue) {
             UserDefaults.standard.setValue(false, forKey: forKey.rawValue)
@@ -68,11 +69,10 @@ class MainMenuViewController: UIViewController {
             UserDefaults.standard.setValue(true, forKey: forKey.rawValue)
             button.setImage(UIImage(named: forKey.rawValue+"_active"), for: .normal)
         }
-        
         UserDefaults.standard.synchronize()
         print(UserDefaults.standard.bool(forKey: forKey.rawValue))
     }
-    
+
     func setButtonImage(forKey: AudioConfig, button: UIButton) {
         if !UserDefaults.standard.bool(forKey: forKey.rawValue) {
             button.setImage(UIImage(named: forKey.rawValue+"_deactive"), for: .normal)
